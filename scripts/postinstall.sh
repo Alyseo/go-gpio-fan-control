0639668f6a008ba6419517a6713bd6cb4547c5ba
Thibaud Demay <thibaud.demay@alyseo.com>
2023-11-22 15:58:44 +0100

    Update install debian and config

--- scripts/postinstall.sh
+++
@@ -1,5 +0,0 @@
-if [ -f "/lib/systemd/system/gpio-fan-control.service" ]; then
-    systemctl daemon-reload
-    systemctl enable gpio-fan-control.service
-    systemctl start gpio-fan-control.service
-fi
